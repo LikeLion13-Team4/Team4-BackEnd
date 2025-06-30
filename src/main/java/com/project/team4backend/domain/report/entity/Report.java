438807f087666e3e432d4131506bb13053ce68e2
tuna1025 <jo215098@gmail.com>
2025-07-01 04:17:30 +0900

    ✨feat: report 도메인 엔티티 생성

--- src/main/java/com/project/team4backend/domain/report/entity/Report.java
+++ src/main/java/com/project/team4backend/domain/report/entity/Report.java
@@ -1,4 +1,29 @@
 package com.project.team4backend.domain.report.entity;
 
+import com.project.team4backend.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reportId;
+
+    private Double totalCalories;
+    private Double totalCarbs;
+    private Double totalProtein;
+    private Double totalFat;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member user;
 }
