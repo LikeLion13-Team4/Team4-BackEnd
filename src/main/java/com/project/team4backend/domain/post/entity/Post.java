ba50b36d4cf96d1cd8c92e2558eb58f56dd7d2a3
tuna1025 <jo215098@gmail.com>
2025-07-01 04:18:08 +0900

    ✨feat: Post 도메인 엔티티 생성

--- src/main/java/com/project/team4backend/domain/post/entity/Post.java
+++ src/main/java/com/project/team4backend/domain/post/entity/Post.java
@@ -1,4 +1,33 @@
 package com.project.team4backend.domain.post.entity;
 
+import com.project.team4backend.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @Column(length = 100)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    private String imageUrl;
+    private Integer likeCount;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Member user;
 }
