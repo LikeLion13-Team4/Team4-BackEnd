714df2054bd489e18a3f4cdad8622c254df5c2de
seohyunseok <tjgustjr33@gmail.com>
2025-07-01 15:53:02 +0900

    ✨feat: Enum 타입 Role과 Gender 생성 및 Member Entity에 Role 필드 추가와 DB에 String 형으로 저장되도록 수정

--- src/main/java/com/project/team4backend/domain/member/entity/Member.java
+++ src/main/java/com/project/team4backend/domain/member/entity/Member.java
@@ -1,4 +1,46 @@
 package com.project.team4backend.domain.member.entity;
 
-public class Member {
+import com.project.team4backend.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name= "birthday", nullable = false)
+    private LocalDate birthday;
+
+    @Column(name= "height", nullable = false)
+    private Double height;
+
+    @Column(name= "weight", nullable = false)
+    private Double weight;
+
+    @Column(name= "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
