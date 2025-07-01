644d3d10c7b83ed9113313c12df0265462f411d3
seohyunseok <tjgustjr33@gmail.com>
2025-07-01 19:18:26 +0900

    ✨feat: create MemberRepository - findByEmail

---
+++ src/main/java/com/project/team4backend/domain/member/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.project.team4backend.domain.member.repository;
+
+import com.project.team4backend.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    @Query("SELECT m FROM Member m WHERE m.email = :email")
+    Optional<Member> findByEmail(@Param("email") String email);
+}
