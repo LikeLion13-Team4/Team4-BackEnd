1dff3dc427876d00c6bf3a71a61a559b3d724039
tuna1025 <jo215098@gmail.com>
2025-07-02 22:14:05 +0900

    ✨feat: Comment에 좋아요 증가, comment 수정 매서드, 추가

--- src/main/java/com/project/team4backend/domain/comment/entity/Comment.java
+++ src/main/java/com/project/team4backend/domain/comment/entity/Comment.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "comment")
-public class Comment{
+public class Comment extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,11 +30,11 @@ public class Comment{
     @Builder.Default
     private Long hierarchy = 0L; // 댓글이면 0 대댓글이면 1이 들어감
 
-    @Column(name = "orders")
+    @Column(name = "comment_orders")
     @Builder.Default
     private Long orders = 0L; // 대댓글 순서로 데이터 정렬할 때 필요
 
-    @Column(name = "groups")
+    @Column(name = "comment_groups")
     private Long groups; // 한개의 댓글과 그에 딸린 대댓글들을 한 그룹으로 묶는다.
 
     @Column(name = "likes")
@@ -48,5 +48,17 @@ public class Comment{
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
+
+    // Comment 내용 업데이트 메서드
+    public void updateComment(String newComment) {
+        if (newComment == null || newComment.trim().isEmpty()) {
+            throw new IllegalArgumentException("댓글 내용은 비어있을 수 없습니다.");
+        }//null이거나 " "도 허용하지 않기 위해 trim().isEmpty(), 유효성검사
+        this.comment = newComment;
+    }
+    // 좋아요 증가
+    public void increaseLikes() {
+        this.likes++;
+    }
 }
 
