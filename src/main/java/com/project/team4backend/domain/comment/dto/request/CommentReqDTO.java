85d1795e450f8fa3fefdf0750c63dea5613c32c1
tuna1025 <jo215098@gmail.com>
2025-07-02 22:16:26 +0900

    ✨feat: 댓글 조회,생성 ResDTO / 댓글 생성, 수정 ReqDTO 작성

--- src/main/java/com/project/team4backend/domain/comment/dto/request/CommentReqDTO.java
+++ src/main/java/com/project/team4backend/domain/comment/dto/request/CommentReqDTO.java
@@ -1,9 +1,23 @@
 package com.project.team4backend.domain.comment.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
 public class CommentReqDTO {
-    public record CommentRequestDto(
-            String comment,
-            Long parentId,
-            Long postId
+
+    // 댓글/대댓글 생성 요청 DTO , 부모댓글id나 게시글 id는 URL로 받는걸로
+    @Builder
+    public record CommentCreateReqDTO(
+            @NotBlank(message = "댓글 내용은 필수입니다.")
+            String comment
     ) {}
+
+    // 댓글 수정 요청 DTO
+    @Builder
+    public record CommentUpdateReqDTO(
+            @NotBlank(message = "수정할 댓글 내용은 필수입니다.")
+            String comment
+    ) {}
+
 }
