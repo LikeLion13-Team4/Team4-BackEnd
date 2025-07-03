afab0708f6631b029a7a191e38bfcde6158c4fd6
tuna1025 <jo215098@gmail.com>
2025-07-04 00:02:04 +0900

    ✨feat: Query service추가

--- src/main/java/com/project/team4backend/domain/meal/service/query/MealQueryService.java
+++ src/main/java/com/project/team4backend/domain/meal/service/query/MealQueryService.java
@@ -1,4 +1,11 @@
 package com.project.team4backend.domain.meal.service.query;
 
+import com.project.team4backend.domain.meal.dto.response.MealResDTO;
+import com.project.team4backend.domain.member.entity.Member;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface MealQueryService {
+    List<MealResDTO.MealRes> getMealsByDate(LocalDate date, Member member);
 }
--- src/main/java/com/project/team4backend/domain/meal/service/query/MealQueryServiceImpl.java
+++ src/main/java/com/project/team4backend/domain/meal/service/query/MealQueryServiceImpl.java
@@ -1,11 +1,29 @@
 package com.project.team4backend.domain.meal.service.query;
 
+import com.project.team4backend.domain.meal.converter.MealConverter;
+import com.project.team4backend.domain.meal.dto.response.MealResDTO;
+import com.project.team4backend.domain.meal.entity.Meal;
+import com.project.team4backend.domain.meal.repository.MealRepository;
+import com.project.team4backend.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MealQueryServiceImpl implements MealQueryService {
+
+    private final MealRepository mealRepository;
+
+    @Override
+    public List<MealResDTO.MealRes> getMealsByDate(LocalDate date, Member member) {
+        List<Meal> meals = mealRepository.findAllByMemberAndDateOrderByMealIdAsc(member, date);
+        return meals.stream()
+                .map(MealConverter::toDTO)
+                .toList();
+    }
 }
