29bd5d1b54ab12b17cd1a665b36e69d72c76d0ef
tuna1025 <jo215098@gmail.com>
2025-07-04 00:01:39 +0900

    ✨feat: command service추가

--- src/main/java/com/project/team4backend/domain/meal/service/command/MealCommandService.java
+++ src/main/java/com/project/team4backend/domain/meal/service/command/MealCommandService.java
@@ -1,4 +1,14 @@
 package com.project.team4backend.domain.meal.service.command;
 
+import com.project.team4backend.domain.meal.dto.request.MealReqDTO;
+import com.project.team4backend.domain.member.entity.Member;
+
+import java.time.LocalDate;
+
 public interface MealCommandService {
+    void createMeal(MealReqDTO.MealReq dto, Member member);
+
+    void updateMeal(Long mealId, MealReqDTO.MealReq req, String email);
+
+    void deleteMeal(Long mealId, String email);
 }
--- src/main/java/com/project/team4backend/domain/meal/service/command/MealCommandServiceImpl.java
+++ src/main/java/com/project/team4backend/domain/meal/service/command/MealCommandServiceImpl.java
@@ -1,11 +1,81 @@
 package com.project.team4backend.domain.meal.service.command;
 
+import com.project.team4backend.domain.meal.converter.MealConverter;
+import com.project.team4backend.domain.meal.dto.request.MealReqDTO;
+import com.project.team4backend.domain.meal.entity.Meal;
+import com.project.team4backend.domain.meal.entity.MealCheck;
+import com.project.team4backend.domain.meal.exception.MealErrorCode;
+import com.project.team4backend.domain.meal.repository.MealCheckRepository;
+import com.project.team4backend.domain.meal.repository.MealRepository;
+import com.project.team4backend.domain.member.entity.Member;
+import com.project.team4backend.domain.member.exception.MemberErrorCode;
+import com.project.team4backend.domain.member.repository.MemberRepository;
+import com.project.team4backend.global.apiPayload.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MealCommandServiceImpl implements MealCommandService {
+
+    private final MealRepository mealRepository;
+    private final MealCheckRepository mealCheckRepository;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void createMeal(MealReqDTO.MealReq dto, Member member) {
+        Meal meal = MealConverter.toEntity(dto, member);
+        mealRepository.save(meal);
+
+        // 자동 출석 체크
+        boolean alreadyChecked = mealCheckRepository.findByMemberAndDate(member, dto.date()).isPresent();
+        if (!alreadyChecked) {
+            MealCheck check = MealCheck.builder()
+                    .date(dto.date())
+                    .isChecked(true)
+                    .member(member)
+                    .build();
+            mealCheckRepository.save(check);
+        }
+    }
+
+    @Override
+    public void updateMeal(Long mealId, MealReqDTO.MealReq req, String email) {
+        Member member = getMemberByEmail(email);
+        Meal meal = mealRepository.findById(mealId)
+                .orElseThrow(() -> new CustomException(MealErrorCode.MEAL_NOT_FOUND));
+
+        if (!meal.getMember().equals(member)) {
+            throw new CustomException(MealErrorCode.UNAUTHORIZED_MEAL_ACCESS);
+        }
+
+        MealConverter.updateEntity(meal, req);
+    }
+
+
+    @Override
+    public void deleteMeal(Long mealId, String email) {
+        Member member = getMemberByEmail(email);
+        Meal meal = mealRepository.findById(mealId)
+                .orElseThrow(() -> new CustomException(MealErrorCode.MEAL_NOT_FOUND));
+
+        if (!meal.getMember().equals(member)) {
+            throw new CustomException(MealErrorCode.UNAUTHORIZED_MEAL_ACCESS);
+        }
+
+        mealRepository.delete(meal);
+    }
+
+
+
+    private Member getMemberByEmail(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(MemberErrorCode.MEMBER_NOT_FOUND));
+    }
+
+
 }
