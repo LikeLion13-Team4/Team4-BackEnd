ca4622817ff9446f02a4178d38abede12fccb940
tuna1025 <jo215098@gmail.com>
2025-07-05 01:33:29 +0900

    ✨feat: report 에러코드 작성

---
+++ src/main/java/com/project/team4backend/domain/report/excption/ReportErrorCode.java
@@ -0,0 +1,20 @@
+package com.project.team4backend.domain.report.excption;
+
+import com.project.team4backend.global.apiPayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+
+@Getter
+@AllArgsConstructor
+public enum ReportErrorCode implements BaseErrorCode {
+
+    EMPTY_MEAL_DATA(HttpStatus.NOT_FOUND, "REPORT_404", "지정한 기간 동안 식단 데이터가 없습니다."),
+    INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "REPORT_400", "시작일은 종료일보다 이전이어야 합니다.")
+    ;
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
