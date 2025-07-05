505a6b639d5ac76ab840a018e8ac37e5da798257
tuna1025 <jo215098@gmail.com>
2025-07-05 23:30:11 +0900

    ✨feat: gptDTO작성

---
+++ src/main/java/com/project/team4backend/domain/report/dto/gpt/GPTReqDTO.java
@@ -0,0 +1,27 @@
+package com.project.team4backend.domain.report.dto.gpt;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+import java.util.List;
+
+public class GPTReqDTO {
+    @Data
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public class GPTRequest {
+        private String model;
+        private List<Message> messages;
+        private int temperature = 1;
+        private int maxTokens = 256;
+        private int topP = 1;
+        private int frequencyPenalty = 0;
+        private int presencePenalty = 0;
+
+        public GPTRequest(String model, String prompt) {
+            this.model = model;
+            this.messages = List.of(new Message("user", prompt));
+        }
+    }
+
+}
---
+++ src/main/java/com/project/team4backend/domain/report/dto/gpt/Message.java
@@ -0,0 +1,4 @@
+package com.project.team4backend.domain.report.dto.gpt;
+
+public class Message {
+}
