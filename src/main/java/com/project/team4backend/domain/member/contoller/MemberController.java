7111162bfbd3b3cddf4b5e1c2250c6c34a268b57
seohyunseok <tjgustjr33@gmail.com>
2025-07-06 16:19:35 +0900

    ✨feat: 회원 정보 조회 api

--- src/main/java/com/project/team4backend/domain/member/contoller/MemberController.java
+++ src/main/java/com/project/team4backend/domain/member/contoller/MemberController.java
@@ -2,11 +2,13 @@ package com.project.team4backend.domain.member.contoller;
 
 import com.project.team4backend.domain.member.repository.MemberRepository;
 import com.project.team4backend.domain.member.service.query.MemberQueryService;
+import com.project.team4backend.global.apiPayload.CustomResponse;
+import com.project.team4backend.global.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,6 +20,13 @@ public class MemberController {
     private final MemberQueryService memberQueryService;
     private MemberRepository memberRepository;
 
+    @Operation(method = "GET", summary = "회원 정보 조회", description = "회원 정보 조회 api입니다.")
+    @GetMapping
+    public CustomResponse<MemberResDTO.MemberPreviewResDTO> getMember(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        return CustomResponse.onSuccess(memberQueryService.getMemberPreview(customUserDetails.getEmail()));
+    }
 
 //    @Operation(description = "내 정보 조회")
 //    @GetMapping()
--- src/main/java/com/project/team4backend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/project/team4backend/domain/member/repository/MemberRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
-    @Query("SELECT m FROM Member m WHERE m.email = :email")
+    @Query("SELECT m FROM Member m WHERE m.email = :email and m.isDeleted = false")
     Optional<Member> findByEmail(@Param("email") String email);
 
     Boolean existsByEmail(String email);
