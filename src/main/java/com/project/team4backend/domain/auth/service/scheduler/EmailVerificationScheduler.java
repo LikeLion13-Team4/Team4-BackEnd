103c098deff48b6cb96af16562ee50bd0d30bd4a
seohyunseok <tjgustjr33@gmail.com>
2025-07-07 18:00:08 +0900

    ♻️refactor: 이메일인증스케줄러 -  EnableScheduling, transactional 애너테이션 추가

--- src/main/java/com/project/team4backend/domain/auth/service/scheduler/EmailVerificationScheduler.java
+++ src/main/java/com/project/team4backend/domain/auth/service/scheduler/EmailVerificationScheduler.java
@@ -5,11 +5,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 @Component
 public class EmailVerificationScheduler {
 
