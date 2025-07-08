32838711c38747a9ec04b5726265d5317dc145c6
seohyunseok <tjgustjr33@gmail.com>
2025-07-08 19:46:28 +0900

    ♻️refactor: 대상에 맞는 예외처리 적용

--- src/main/java/com/project/team4backend/domain/post/service/command/PostCommandServiceImpl.java
+++ src/main/java/com/project/team4backend/domain/post/service/command/PostCommandServiceImpl.java
@@ -1,17 +1,22 @@
 package com.project.team4backend.domain.post.service.command;
 
 import com.project.team4backend.domain.member.entity.Member;
+import com.project.team4backend.domain.member.exception.MemberErrorCode;
+import com.project.team4backend.domain.member.exception.MemberException;
 import com.project.team4backend.domain.member.repository.MemberRepository;
 import com.project.team4backend.domain.post.converter.PostConverter;
 import com.project.team4backend.domain.post.dto.reponse.PostResDTO;
 import com.project.team4backend.domain.post.dto.request.PostReqDTO;
 import com.project.team4backend.domain.post.entity.Post;
+import com.project.team4backend.domain.post.entity.PostImage;
 import com.project.team4backend.domain.post.exception.PostErrorCode;
 import com.project.team4backend.domain.post.exception.PostException;
 import com.project.team4backend.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -39,7 +44,7 @@ public class PostCommandServiceImpl implements PostCommandService {
     @Override
     public PostResDTO.PostUpdateResDTO updatePost(Long postId, PostReqDTO.PostUpdateReqDTO dto, String email) {
         Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new PostException(PostErrorCode.MEMBER_NOT_FOUND));
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
 
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
@@ -49,7 +54,8 @@ public class PostCommandServiceImpl implements PostCommandService {
         }
 
         // update는 Post 엔티티 내부 메서드로 실행
-        post.update(dto.title(), dto.content(), dto.tags(), dto.images());
+        List<PostImage> images = PostConverter.toImageEntities(dto.images(), post);
+        post.update(dto.title(), dto.content(), dto.tags(), images);
 
         return PostConverter.toUpdateDTO(post);
     }
@@ -58,7 +64,7 @@ public class PostCommandServiceImpl implements PostCommandService {
     public PostResDTO.PostDeleteResDTO deletePost(Long postId, String email) {
         // 1. 사용자 조회
         Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new PostException(PostErrorCode.MEMBER_NOT_FOUND));
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
 
         // 2. 게시글 조회 (작성자와 이미지 fetch)
         Post post = postRepository.findById(postId)
