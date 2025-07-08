8049ce6e2f00ba7262da72520ead56f9b1f64ed1
seohyunseok <tjgustjr33@gmail.com>
2025-07-08 14:34:37 +0900

    ✨feat: ImageConverter

---
+++ src/main/java/com/project/team4backend/domain/image/converter/ImageConverter.java
@@ -0,0 +1,9 @@
+package com.project.team4backend.domain.image.converter;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ImageConverter {
+
+}
