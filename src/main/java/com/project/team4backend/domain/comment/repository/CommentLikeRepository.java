7da15d7cfc64f06d200547ddb5a1f1a7c4c7f3e4
tuna1025 <jo215098@gmail.com>
2025-07-09 19:46:07 +0900

    🐛fix: 게시판 삭제시 댓글이 달려있으면 삭제되지 않는 버그 수정

--- src/main/java/com/project/team4backend/domain/comment/repository/CommentLikeRepository.java
+++ src/main/java/com/project/team4backend/domain/comment/repository/CommentLikeRepository.java
@@ -5,7 +5,11 @@ import com.project.team4backend.domain.comment.entity.CommentLike;
 import com.project.team4backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+
+    void deleteAllByCommentIn(List<Comment> comments);
 
     boolean existsByMemberAndComment(Member member, Comment comment);
 }
